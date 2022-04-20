05f5615df9a9844bcf63b9417b2635170a725f9c
Jamel Scott Fadel <itsjamelscott@gmail.com>
2022-04-20 00:51:10 -0700

    removed <img> tags and apostrophe

--- pages/about.js
+++ pages/about.js
@@ -6,7 +6,7 @@ export default function About() {
   return (
       <div className={styles.container}>
         <div className={styles.aboutLeft}>
-        <img className='waveEmoji' src="/Emoji.png" alt="waving hand emoji" width="40"/>
+        <Image className='waveEmoji' src="/Emoji.png" alt="waving hand emoji" width="40" height="40"/>
           <h1 className={styles.aboutText} style={{fontSize: "30px"}}>Hello, Im Jamel.</h1>
           <h1 className={styles.aboutText} style={{fontSize: "100px"}}>Front-End</h1>
           <h1 className={styles.aboutText} style={{fontSize: "100px"}}>Web Developer</h1>
@@ -14,7 +14,7 @@ export default function About() {
         </div>
         <div className={styles.aboutRight}>
           <Image
-          alt="a photo of jamel with some graphics"
+          alt="a photo of jamel with graphics"
           src="/group-11.png"
           width="400px"
           height="400px"
