7ca9fe9b260d2530ab1810f1b4b727928b9de391
Jamel Scott Fadel <itsjamelscott@gmail.com>
2022-04-22 01:05:03 -0700

    slight styling updates to colours and transitions

--- pages/_app.js
+++ pages/_app.js
@@ -4,22 +4,21 @@ import Head from 'next/head';
 import Footer from '../components/footer';
 import About from './about';
 import Languages from './languages';
-import Index from './index'
+import Index from './index';
 import Contact from './contact';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-        <div style={{backgroundColor: "#293241", height: "100%", }}>
+      <div style={{ backgroundColor: '#293241', height: '100%' }}>
         <Navbar />
-        <About />
-        <Index />
-        {/* <Component {...pageProps} /> */}
-        <Languages />
-        <Contact />
+          <About />
+          <Index />
+          {/* <Component {...pageProps} /> */}
+          <Languages />
+          <Contact />
         <Footer />
       </div>
-
     </>
   );
 }
--- pages/languages.js
+++ pages/languages.js
@@ -1,46 +1,37 @@
-import { languages } from "../projects";
-import Image from "next/image";
-import styles from '../styles/Languages.module.css'
+import { languages } from '../projects';
+import Image from 'next/image';
+import styles from '../styles/Languages.module.css';
 
 function Languages() {
+  const mappedImages = languages.map((element, idx) => {
+    return (
+      <div key={`logo-${idx}`}>
+        <Image
+          className={styles.logos}
+          alt={element.name}
+          src={element.src}
+          width="115px"
+          height="115px"
+        />
+      </div>
+    );
+  });
 
-    const mappedImages = languages.map((element, idx)=>{
-
-        return (
-            <div key={`logo-${idx}`}>
-                <Image
-                className={styles.logos}
-                alt={element.name}
-                src={element.src}
-                width='115px'
-                height='115px'
-                 />
-            </div>
-        )
-        
-        })
-
-    return ( 
-        <div className={styles.languageContainer} >
-        <h2>Languages/Dev tools:</h2>
-        <div className={styles.languages}>
+  return (
+    <div className={styles.languageContainer}>
+      <h2>Languages/Dev tools:</h2>
+      <div className={styles.languages}>
         {mappedImages}
         <Image
-        className={styles.logos}
-        alt="figma-logo"
-        src='/languages_icons/figma.png'
-        width='75px'
-        height={"100px"}
+          className={styles.logos}
+          alt="figma-logo"
+          src="/languages_icons/figma.png"
+          width="75px"
+          height={'100px'}
         />
-        </div>
-        </div>
-     );
+      </div>
+    </div>
+  );
 }
 
 export default Languages;
-
-
-// {
-//     name: "figma-logo",
-//     src: '/languages_icons/figma.png'
-//   },
